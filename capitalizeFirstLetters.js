7aed13617c565925787866b4b49408a5d3ca1f89
Ornella Gatto <ornellagatto7@gmail.com>
2021-03-21 13:50:49 +0100

    git tag step-1

--- capitalizeFirstLetters.js
+++ capitalizeFirstLetters.js
@@ -1,11 +1,7 @@
-const assert = require('assert');
+function capitalizeFirstLetters(input) {
+    return input.length > 0
+        ? input[0].toUpperCase() + input.slice(1)
+        : '';
+}
 
-assert.strictEqual(typeof capitalizeFirstLetters, 'function');
-
-assert.strictEqual(capitalizeFirstLetters.length, 1);
-
-assert.strictEqual(capitalizeFirstLetters('javaScript'), 'JavaScript');
-
-assert.strictEqual(capitalizeFirstLetters('z'), 'Z');
-
-assert.strictEqual(capitalizeFirstLetters(''), '');
+module.exports = capitalizeFirstLetters;
